ae346554d13cc06f38a6935178614b478f73eeb1
DAOZ <orellanadaniel461@gmail.com>
2025-11-15 23:15:02 -0600

    creando tabla

--- EventWall/appEventWall/models.py
+++ EventWall/appEventWall/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Evento(models.Model):
+    titulo = models.CharField(max_length=120)
+    descripcion = models.TextField(blank=True)
+    fecha = models.DateField()
+    hora = models.TimeField(blank=True, null=True)
+    lugar = models.CharField(max_length=100, blank=True)
+    tipo = models.CharField(max_length=50, blank=True)
+
+    def __str__(self):
+        return self.titulo
